65218309d0a6e434f2cbce459e75f8938e70b672
angeelgod <zubzero2000@gmail.com>
2020-02-20 12:57:21 -0300

    Change layout and color for Weather assignment Added weather icon Applied Bulma's classes to first column (info)

--- apis_ajax/weather/script.js
+++ apis_ajax/weather/script.js
@@ -5,13 +5,15 @@ $(document).ready(function(){
 		if (unit==='default'){unit='';u='K'}
 		else if (unit==='imperial'){u='°F'}
 		else {u='°C'}
-
+		$('#city').removeClass('is-primary').addClass('is-success')
 		$.getJSON("https://api.openweathermap.org/data/2.5/weather?q="+$('#city').val()+"&appid=4eb0b682bf2c611d898d072534723ea4&units="+unit,function(data){
-			$('p#city_name').text(data.name)
-			$('p#current_temp').text(data.main.temp+' '+u)
+			$('p#city_name').text(data.name);
+			$('p#current_temp').text(data.main.temp+' '+u);
+			$('img#current_icon').attr('src','https://openweathermap.org/img/wn/'+data.weather[0].icon+'@2x.png');
 			
 
 		});
+		$('#weather').show('slow');
 		return false;
 	});
 });
