6f2e2d1b87c37d8a1534cd0dd3479e022627af30
angeelgod <zubzero2000@gmail.com>
2020-02-27 13:15:17 -0300

    Add files for MySQL assignments

---
+++ sql_erd/ninjas_dojos.sql
@@ -0,0 +1,8 @@
+use ninjasdojos;
+INSERT INTO ninjas(name, ability, Dojo_id)
+VALUES ('victoria','morir',1),
+('Gustavo','mm',2);
+SELECT * FROM ninjas;
+UPDATE ninjas
+SET ability='SER WETa'
+WHERE id=2;
