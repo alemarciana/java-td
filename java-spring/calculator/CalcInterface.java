0432fd21a58faf4504633a03a79d6795c46e89c6
angeelgod <zubzero2000@gmail.com>
2020-04-23 17:34:32 -0400

    Add files for calculator assignment

---
+++ java-spring/calculator/CalcInterface.java
@@ -0,0 +1,8 @@
+package calculator;
+public interface CalcInterface {
+	public void performOperation();
+	public void setOperandOne(double operand);
+	public void setOperation(String op);
+	public void setOperandTwo(double operand);
+	public double getResult();
+}
